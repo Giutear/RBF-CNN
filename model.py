84820d3269e93be7bfa89ffe01c166e6ab67255e
Giutear <75430192+Giutear@users.noreply.github.com>
2021-01-19 16:18:20 +0100

    Update model.py
    Started working on the model class which will ultimately used to create the network with all it's different layers.

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
+import numpy as np
+import math
+from rbfConvolution import RBF_Convolution
 
+class Model():
+  
+  def __init__(self):
+    self.layer = []
+    
+  def display(self):
+    print("This model is using the following layout:"
+    
+  def addConvolution(self):
+    self.layer.append(Rbf_Convolution())
+    
