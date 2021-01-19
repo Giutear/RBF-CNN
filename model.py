8379603e4c48ba09c51ebbefcf446db1bccd26e0
Giutear <gsawczak@gmx.de>
2021-01-19 22:43:06 +0100

    Update model.py
    Refactored the code since it didn't use the right indents.

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ from rbfConvolution import RBF_Convolution
 
 class Model():
   
-  def __init__(self):
-    self.layer = []
+    def __init__(self):
+        self.layer = []
     
-  def display(self):
-    print("This model is using the following layout:"
+    def display(self):
+        print("This model is using the following layout:"
     
-  def addConvolution(self):
-    self.layer.append(Rbf_Convolution())
+    def addConvolution(self):
+        self.layer.append(Rbf_Convolution())
     
